ffe6b1f7aa9afa04f2e21cc1cfee342ffd84289f
SimpleSenpaiii <80858169+SimpleSenpaiii@users.noreply.github.com>
2021-03-17 17:50:27 -0400

    Add files via upload

---
+++ Genesis Conversation.py
@@ -0,0 +1,23 @@
+greeting_1 = f'A human huh? I guess I could try and talk to one'
+print(greeting_1)
+first_name = input("First off what's your first name? ")
+print(first_name)
+question_1 = f" {first_name}? huh I like it... but uhhh you got a last name don't ya? "
+print(question_1)
+last_name = input("So lemme hear it")
+print(last_name)
+feedback_1 = f' oooooo the almighty {first_name} {last_name} I like the sound of that'
+print(feedback_1)
+genesis_name_pt1 = f" well by now you're probably wondering who I am"
+print(genesis_name_pt1)
+genesis_name_pt2 = f" To answer that question you dear {first_name} are speaking to the one and only Genesis"
+print(genesis_name_pt2)
+genesis_quote_1 = f" Charmed i'm sure "
+print(genesis_quote_1)
+question_2 = f" Hey {first_name} what year were you born in if you don't mind me asking? "
+print(question_2)
+birth_year = input(question_1)
+age = 2021 - int(birth_year)
+usr_age = f" So you're {age} Years old? "
+print(usr_age)
+print("cool")
